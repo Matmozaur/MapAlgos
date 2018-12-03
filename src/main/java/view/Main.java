5cfae34eaf21c096ef3226ae61ca7720af4e8fe3
Matmozaur <matmozaur@gmial.com>
2018-12-03 22:12:43 +0100

    Refactoring

--- src/main/java/view/Main.java
+++ src/main/java/view/Main.java
@@ -10,7 +10,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Main {
-
+    //random comment
 	private static Color vcolor = Color.BLACK;
 	/**
 	 * default color of edges
